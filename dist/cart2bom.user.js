25ae8f545717ed3cd8eee07f7f5b3c59bbcac4bb
morita_masato <mori.masa.1345.no.2@gmail.com>
2026-08-11 20:12:08 +0900

    chore(release): 0.5.24へ更新

--- dist/cart2bom.user.js
+++ dist/cart2bom.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cart2BOM
 // @namespace    cart2bom
-// @version      0.5.23
+// @version      0.5.24
 // @author       morita_masato
 // @description  通販サイトのカートを保存・共有・再利用します
 // @homepageURL  https://github.com/masa2429/cart2bom
